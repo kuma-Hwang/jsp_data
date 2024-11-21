91734bbc7c9b005491758afe10995ac15a97eef2
kuma <kumma0807@gmail.com>
2024-11-21 16:31:14 +0900

    포워드 액션코드 사용 실습

---
+++ myforward/src/main/java/com/company/ex03/FirstServlet.java
@@ -0,0 +1,26 @@
+package com.company.ex03;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.jasper.tagplugins.jstl.core.Out;
+
+@WebServlet("/first_3")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("<script type='text/javascript'>");
+		out.print("location.href='second_3';");
+		out.print("</script>");
+	}
+
+}
